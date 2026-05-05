41ef949d44a710d9992ce0f50ccaa8f764d0e2e4
mizan <info@sabq.org>
2026-05-05 11:21:00 +0300

    feat: connect to Neon, add db scripts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from 'drizzle-kit';
+import { config } from 'dotenv';
+
+// تحميل .env.local
+config({ path: '.env.local' });
 
 export default {
   schema: './src/lib/db/schema.ts',
